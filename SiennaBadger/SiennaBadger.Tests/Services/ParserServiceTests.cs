53f6a008d355814b7ee63b4ee909f799e66e52ca
laytonc <laytonc@hotmail.com>
2018-03-21 06:56:50 -0400

    fix release issue.

--- SiennaBadger/SiennaBadger.Tests/Services/ParserServiceTests.cs
+++ SiennaBadger/SiennaBadger.Tests/Services/ParserServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.Extensions.Logging;
 using NSubstitute;
 using SiennaBadger.Infrastructure.Services;
 using Xunit;
@@ -8,13 +10,15 @@ namespace SiennaBadger.Tests.Services
     public class ParserServiceTests
     {
         [Fact]
-        public void ParsePageAsync_ShouldReturnPageSummary()
+        public async Task ParsePageAsync_ShouldReturnPageSummary()
         {
             // arrange
             var logger = Substitute.For<ILogger<ParserService>>();
+            var url = "https://wahoo-siennabadger-dev.azurewebsites.net/html/test-basic.html";
 
             // act
             var sut = new ParserService(logger);
+            //var result = await sut.ParsePageAsync(url);
 
             // assert
 

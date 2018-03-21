8b2c4347eb0df48cc1e4a7e4d4254ba99e56e246
laytonc <laytonc@hotmail.com>
2018-03-21 07:27:08 -0400

    Add ParserService test

--- SiennaBadger/SiennaBadger.Tests/Services/ParserServiceTests.cs
+++ SiennaBadger/SiennaBadger.Tests/Services/ParserServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using SiennaBadger.Data.Models;
 using SiennaBadger.Infrastructure.Services;
 using Xunit;
 
@@ -9,6 +13,9 @@ namespace SiennaBadger.Tests.Services
 {
     public class ParserServiceTests
     {
+        // Note, these are integration tests, for unit tests additional refactoring is required to abstract
+        // some pieces of the AngleSharp BrowsingContext
+
         [Fact]
         public async Task ParsePageAsync_ShouldReturnPageSummary()
         {
@@ -18,10 +25,16 @@ namespace SiennaBadger.Tests.Services
 
             // act
             var sut = new ParserService(logger);
-            //var result = await sut.ParsePageAsync(url);
+            var result = await sut.ParsePageAsync(url);
 
             // assert
-
+            // basic checks that we are getting a valid page summary object
+            result.Should().NotBeNull();
+            result.Should().BeOfType<PageSummary>();
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.WordCount.Should().BeGreaterThan(0);
+            result.Words.Count().Should().BeGreaterThan(0);
+            result.Images.Count().Should().BeGreaterThan(0);
         }
     }
 }
